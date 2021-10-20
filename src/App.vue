e0a1e6cdc18dfadabd4b7ee8756984db310fc1a8
nickypangers <nixonpang98@gmail.com>
2021-10-21 02:28:47 +0800

    chat bubble style

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,39 @@
 <template>
-  <input type="text" v-model="message" class="border border-black" />
-  <button
-    class="button bg-blue-300 px-4 py-2"
-    @click="sendMessage"
-    :disabled="socket == null"
-  >
-    Hi
-  </button>
-  <div>
-    <div
-      class="border"
-      v-for="(chatMessage, index) in chatMessages"
-      :key="'chat-' + index"
-    >
-      <p class="text-start">
-        {{ chatMessage.time }}
-      </p>
-      <p class="text-start">
-        {{ chatMessage.message }}
-      </p>
+  <div v-if="!isConnected">
+    <input class="border border-black" type="text" v-model="address" />
+    <button class="bg-blue-200 px-2 py-1" @click="connectToChat">
+      Connect
+    </button>
+  </div>
+  <div v-if="isConnected">
+    <div class="mt-3 w-full mt-12 container">
+      <ChatBubble
+        v-for="(chatMessage, index) in chatMessages"
+        :chat="chatMessage"
+        :key="'chat-' + index"
+      />
     </div>
+    <input type="text" v-model="message" class="border border-black" />
+    <button
+      class="button bg-blue-300 px-4 py-2"
+      @click="sendMessage"
+      :disabled="socket == null"
+    >
+      Hi
+    </button>
   </div>
 </template>
 
 <script>
 import { onMounted, computed, ref } from "vue";
 import { useStore } from "vuex";
-// import { sendMessage as send, decodeMessage } from "@/helpers/socket.js";
+import ChatBubble from "@/components/ChatBubble.vue";
 
 export default {
   name: "App",
+  components: {
+    ChatBubble,
+  },
   setup() {
     let store = useStore();
 
@@ -39,6 +43,8 @@ export default {
 
     const chatMessages = ref([]);
 
+    const address = ref("");
+
     const sendMessage = () => {
       if (!message.value) {
         return;
@@ -47,16 +53,27 @@ export default {
       message.value = "";
     };
 
-    onMounted(() => {
+    const isConnected = ref(false);
+
+    const connectToChat = () => {
+      if (!address.value) {
+        console.log("address cannot be empty");
+        return;
+      }
       let newSocket = new WebSocket(
-        "ws://" + window.location.hostname + ":3000/ws?address=testing"
+        // "ws://" + window.location.hostname + ":3000/ws?address=" + address.value
+        "ws://gowschatserver.herokuapp.com/ws?address=" + address.value
       );
+
       console.log("Attempting WebSocket Connection");
 
       newSocket.onopen = () => {
-        console.log("Successfully connected");
         store.commit("setSocket", newSocket);
-        socket.value.send("hello");
+        isConnected.value = true;
+        console.log("Successfully connected");
+
+        store.commit("setAddress", address.value);
+        address.value = "";
       };
 
       newSocket.onmessage = (msg) => {
@@ -69,9 +86,14 @@ export default {
         console.log("Closed connection: ", event);
         store.commit("setSocket", null);
       };
-    });
+    };
+
+    onMounted(() => {});
 
     return {
+      address,
+      isConnected,
+      connectToChat,
       sendMessage,
       message,
       socket,
--- src/components/ChatBubble.vue
+++ src/components/ChatBubble.vue
@@ -1,11 +1,65 @@
 <template>
-  <div class="font-bold"></div>
+  <div class="flex items-center" :class="rowClass">
+    <div class="bubble" :class="bubbleClass">
+      <p v-if="!isSelf">{{ chat.address }}</p>
+      <div class="bubble-content" :class="{ 'mt-2': !isSelf }">
+        <p>{{ chat.message }}</p>
+        <p class="text-sm text-gray-600 ml-2">{{ chat.displayTime }}</p>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
+import { computed } from "vue";
+import { useStore } from "vuex";
+
 export default {
   props: {
     chat: Object,
   },
-  setup() {},
+  setup(props) {
+    const store = useStore();
+
+    const isSelf = computed(() => props.chat.address == store.state.address);
+
+    const rowClass = computed(() => {
+      return {
+        "justify-end": isSelf.value,
+        "justify-start": !isSelf.value,
+      };
+    });
+
+    const bubbleClass = computed(() => {
+      return {
+        self: isSelf.value,
+        other: !isSelf.value,
+      };
+    });
+
+    return {
+      rowClass,
+      isSelf,
+      bubbleClass,
+    };
+  },
 };
 </script>
+
+<style scoped>
+.bubble {
+  @apply px-3 pt-3 py-2 my-0.5 rounded-xl text-left;
+  min-width: 4rem;
+}
+
+.bubble-content {
+  @apply flex items-center justify-between items-end;
+}
+
+.self {
+  @apply bg-blue-300;
+}
+
+.other {
+  @apply bg-green-300;
+}
+</style>

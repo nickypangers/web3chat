cdc254f6e580865d8357d9992a1ba9a3e2d44eba
Nixon <nixon.p@mosaicdigi.com>
2021-10-20 18:52:46 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,82 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <input type="text" v-model="message" class="border border-black" />
+  <button
+    class="button bg-blue-300 px-4 py-2"
+    @click="sendMessage"
+    :disabled="socket == null"
+  >
+    Hi
+  </button>
+  <div>
+    <div
+      class="border"
+      v-for="(chatMessage, index) in chatMessages"
+      :key="'chat-' + index"
+    >
+      <p class="text-start">
+        {{ chatMessage.time }}
+      </p>
+      <p class="text-start">
+        {{ chatMessage.message }}
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import { onMounted, computed, ref } from "vue";
+import { useStore } from "vuex";
+// import { sendMessage as send, decodeMessage } from "@/helpers/socket.js";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  setup() {
+    let store = useStore();
+
+    const socket = computed(() => store.state.socket);
+
+    const message = ref("");
+
+    const chatMessages = ref([]);
+
+    const sendMessage = () => {
+      if (!message.value) {
+        return;
+      }
+      socket.value.send(message.value);
+      message.value = "";
+    };
+
+    onMounted(() => {
+      let newSocket = new WebSocket(
+        "ws://" + window.location.hostname + ":3000/ws"
+      );
+      console.log("Attempting WebSocket Connection");
+
+      newSocket.onopen = () => {
+        console.log("Successfully connected");
+        store.commit("setSocket", newSocket);
+        socket.value.send("hello");
+      };
+
+      newSocket.onmessage = (msg) => {
+        let object = JSON.parse(msg.data);
+        console.log(object);
+        chatMessages.value.push(object);
+      };
+
+      newSocket.onclose = (event) => {
+        console.log("Closed connection: ", event);
+        store.commit("setSocket", null);
+      };
+    });
+
+    return {
+      sendMessage,
+      message,
+      socket,
+      chatMessages,
+    };
   },
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,14 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
-  mutations: {},
+  state: {
+    socket: null,
+  },
+  mutations: {
+    setSocket(state, newSocket) {
+      state.socket = newSocket;
+    },
+  },
   actions: {},
   modules: {},
 });
